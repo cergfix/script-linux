51beeb0c1ce5ab70f70eb062ebc62bf8c9965ce7
Sergei Selivanov <cergfix@gmail.com>
2018-10-22 15:53:41 +0300

    Adding escaping for double quotes for command example

--- find_r.sh
+++ find_r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$1" = "" || "$2" = "" || "$3" = "" ]]; then
-    echo "Example: ./find_r.sh "." "*.php" "teststring""
+    echo "Example: ./find_r.sh \".\" \"*.php\" \"teststring\""
     exit 1
 else
     echo "find $1 -name \"$2\" -print0 | xargs -0 grep -n -H \"$3\""
